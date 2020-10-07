fdf083c03a695c43f1715d6649a7d2fa46998443
ashfaqhaq <44408323+ashfaqhaq@users.noreply.github.com>
2020-10-08 03:16:37 +0530

    first attempt

--- script.js
+++ script.js
@@ -2,40 +2,51 @@
   "usestrict";
   
   let button = document.querySelector('.button');
-  let inputValue = document.querySelector('.inputValue');
+  let inputValue = document.querySelector('#inputValue');
+  console.log(inputValue)
   let name = document.querySelector('.name');
   let desc = document.querySelector('.desc');
   let temp = document.querySelector('.temp');
   let sunrise = document.querySelector('.sunrise');
+  let sunset = document.querySelector('.sunset');
+  let wind = document.querySelector('.wind');
   const key = 'd8cbffb4fff02bdd85b50c90da1022a3';
+  let contentBox=document.querySelector(".content-box");
+
 
   button.addEventListener('click', function(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputValue.value}&appid=${key}`)
     .then(response => response.json())
      .then(data => {
        console.log(data);
-        var nameValue = data.name;
-        var descValue = data['weather'][0]['description'];
-        var tempValue = data['main']['temp'];
-        var sunriseValue = convertUnixTime(data.sys.sunrise);
+        const  nameValue = data.name;
+        const  descValue = data['weather'][0]['description'];
+        const tempValue = data['main']['temp'];
+        const windValue = data['wind']['speed'];
+        const sunriseValue = convertUnixTime(data.sys.sunrise);
+        const sunsetValue = convertUnixTime(data.sys.sunset);
         name.textContent = nameValue;
         desc.textContent = descValue;
         temp.textContent = `${tempValue-273.15} °C`;
+        wind.textContent = windValue;
         sunrise.textContent = sunriseValue;
+        sunset.textContent = sunsetValue;
+         contentBox.style.display="block";
+
       }
     )
 
-  .catch(err => alert('Wrong City'));
+  .catch(err => console.log(err));
   });
 
   function convertUnixTime(unix_timestamp){
-    var date = new Date(unix_timestamp * 1000);
+    let date = new Date(unix_timestamp * 1000);
     // Hours part from the timestamp
-    var hours = date.getHours();
+    let hours = date.getHours();
     // Minutes part from the timestamp
-    var minutes = "0" + date.getMinutes();
+    let minutes = "0" + date.getMinutes();
     // Seconds part from the timestamp
-    var seconds = "0" + date.getSeconds();
+    let seconds = "0" + date.getSeconds();
 
     // Will display time in 10:30:23 format
     return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
